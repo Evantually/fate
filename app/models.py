fb79258fe5904535dbb19c60e5b70cec28fc0ffe
Evantually <egbeesefl@gmail.com>
2019-10-17 07:57:50 -0500

    email non-unique

--- app/models.py
+++ app/models.py
@@ -84,7 +84,7 @@ class ProfessionItem(SearchableMixin, db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120), index=True)
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
